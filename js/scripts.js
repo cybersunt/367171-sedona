229ea61b071f4d24fd68260aef7fb245387e1fd5
Yulia <juliam2007@mail.ru>
2019-03-31 00:07:49 +0300

    Добавление карты на сайт

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//modal window
 (function () {
 
   var searchBox = document.querySelector('.search-box');
@@ -12,3 +13,26 @@
     searchForm.classList.toggle('hidden');
   })
 })();
+
+//yandex-map
+ymaps.ready(function(){
+
+  var map = new ymaps.Map('map', {
+    center: [34.81029621, -111.76329386],
+    zoom: 9,
+    controls: ['zoomControl'],
+    behaviors: ['drag']
+  });
+
+  var placemark = new ymaps.Placemark(['34.89', '-111.77'], {
+    preset: 'islands#icon',
+    iconColor: '#0095b6'
+  });
+
+  map.geoObjects.add(placemark);
+
+  //hide map image
+  (function () {
+    document.querySelector('.map__img').classList.add('hidden');
+  })();
+});
